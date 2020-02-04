d1f0d61749d298e8dadb83569fc12dfda213068e
looping-bell-1500 <gabrielalcantara7@yahoo.com>
2020-02-04 19:20:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -16,10 +16,6 @@ def move(board, index, character = "X")
   return board[index] 
 end
 
-board = [" ", " ", " "]
-def update_array_at_with(array, index, value)
-  array[index] = value
-end
  
 update_array_at_with(board, 0, "X")
 
